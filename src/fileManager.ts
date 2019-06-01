adfff096b27ff8f06ad317560e7f1782023a79e7
Seungyong Kwak <guieenoutis@gmail.com>
2019-06-01 16:06:18 +0900

    Refactor codes of managing files

--- src/fileManager.ts
+++ src/fileManager.ts
@@ -1,43 +1,109 @@
-import fs from 'fs-extra'
-import path from 'path'
+import fs from 'fs';
+import path from 'path';
 
-export const copyFileSync = (source: string, destDirPath: string) => {
-    let targetFile = destDirPath
+type ErrorProneFunction = (e?: any) => any;
+type CustomFunction = () => any;
 
-    if (fs.existsSync(destDirPath)) {
-        targetFile = path.join(destDirPath, path.basename(source))
+// https://stackoverflow.com/a/14387791/5260068
+function copyFile(srcFile: fs.PathLike, dstFile: fs.PathLike, cb: ErrorProneFunction) {
+    // If file doesn't exist
+    if (fs.existsSync(srcFile) === false) {
+        return cb(`[copyFile] File does not exist:: ${srcFile}`);
     }
 
-    fs.writeFileSync(targetFile, fs.readFileSync(source))
-}
-
-const ignore = {
-    'src/copyFolder.ts': true,
-    'src/starter.ts': true,
-    '.git': true,
-    node_modules: true,
-    build: true,
-}
-
-export const getFileList = (targetPath: string, list: string[] = [], base: string = ''): string[] => {
-    let tmp = fs.readdirSync(targetPath).map(e => path.join('', e))
-    for (let i = 0; i < tmp.length; i++) {
-        let currentPath = tmp[i]
-        if (ignore[currentPath]) {
-            tmp.splice(i, 1)
-            i--
-            continue
-        }
-
-        if (fs.lstatSync(path.join(targetPath, currentPath)).isDirectory()) {
-            tmp.splice(i, 1)
-            const tmpLen = tmp.length
-            tmp = getFileList(path.join(targetPath, currentPath), tmp, currentPath)
-
-            i = tmp.length > tmpLen ? tmp.length - 1 : i - 1
+    const destDirPath = path.dirname(dstFile as string);
+    if (fs.existsSync(destDirPath) === false) {
+        console.log(`[copyFile] Dest file directory does not exist:: ${dstFile} :: ${destDirPath}`);
+        try {
+            fs.mkdirSync(destDirPath);
+        } catch (e) {
+            return cb(`[copyFile] Failed to create file:: ${e}`);
         }
     }
 
-    list = [...list, ...tmp.map(e => path.join(base, e))]
-    return list
+    let rd = fs.createReadStream(srcFile);
+    rd.on('data', function(chunk) {
+        // console.log('[copyFile] chunk : ', chunk);
+    });
+    rd.on('error', function(err) {
+        // console.log('[copyFile] rd: err', err);
+        done(err);
+    });
+
+    // rd.on('close', function() {
+    // ReadStream has been destroyed and file has been closed
+    // console.log('[copyFile] rd: close');
+    // })
+
+    let wr = fs.createWriteStream(dstFile);
+    wr.on('error', function(err) {
+        // console.log('[copyFile] wr: err', err);
+        done(err);
+    });
+    wr.on('close', function() {
+        // console.log('[copyFile] wr: close');
+        done();
+    });
+
+    // pipe
+    rd.pipe(wr);
+
+    function done(err?: any) {
+        cb(err);
+        rd.destroy();
+        wr.destroy();
+    }
 }
+
+// ref: https://stackoverflow.com/a/49601340/5260068
+function readFilesRecursivelySync(dir: string) {
+    const files: [[string, string]?] = [];
+    const mapDir = {};
+
+    const readFile = (dir: fs.PathLike, prefix: null | string = null) => {
+        fs.readdirSync(dir).forEach(filename => {
+            // console.log(' filename :: ', filename);
+            // const name = path.parse(filename).name;
+            // const ext = path.parse(filename).ext;
+            const filepath = path.resolve(dir as string, filename);
+            const stat = fs.statSync(filepath);
+            const isFile = stat.isFile();
+            const filenameWithPrefix = prefix === null ? filename : `${prefix}/${filename}`;
+
+            if (isFile) {
+                let tmp = [];
+                if (prefix) {
+                    files.push();
+                }
+                files.push([filepath, filenameWithPrefix]);
+            } else {
+                // console.log(filepath);
+                if (mapDir[filepath] === undefined) {
+                    mapDir[filepath] = true;
+                    readFile(filepath, filenameWithPrefix);
+                }
+            }
+        });
+    };
+
+    readFile(dir);
+
+    return files;
+}
+
+function copyFiles(srcDir: string, destDir: string, cb: null | CustomFunction = null) {
+    const fileListToCopy = readFilesRecursivelySync(srcDir);
+
+    fileListToCopy.forEach(file => {
+        copyFile(file![0], destDir + '/' + file![1], e => {
+            if (e) {
+                console.log('Error occurred while copy files:: ', e);
+                return;
+            }
+        });
+    });
+
+    cb && cb();
+}
+
+export { copyFiles };
